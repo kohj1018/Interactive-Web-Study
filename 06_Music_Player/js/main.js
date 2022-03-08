44c6770c1063f4c32bdaec787f3d07b5045408aa
kohj1018 <kohj1018@gmail.com>
2022-03-08 20:14:46 +0900

    전체 페이지 index 생성

--- 06_Music_Player/js/main.js
+++ 06_Music_Player/js/main.js
@@ -8,7 +8,7 @@ for (let el of lists) {
   let pic = el.querySelector(".pic");
   // 각 article 요소를 45도씩 회전하고 아래로 배치
   el.style.transform = `rotate(${deg*i}deg) translateY(-100vh)`;
-  pic.style.backgroundImage = `url(../img/member${i+1}.jpg)`; /* 배경 이미지 추가 */
+  pic.style.backgroundImage = `url(img/member${i+1}.jpg)`; /* 배경 이미지 추가 */
   i++;
 
   // 각 article 요소 안쪽의 재생, 정지, 처음부터 재생 버튼을 변수에 저장
